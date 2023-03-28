2765a5ab6ba5eb50be51eda922229306cc89af97
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-03-28 17:25:18 -0500

    done

--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 
 from app.db import database, User
 
+from fastapi_keycloak import FastAPIKeycloak, OIDCUser
+from fastapi.responses import RedirectResponse
 
-app = FastAPI(title="FastAPI, Docker, and Traefik")
+
+app = FastAPI(title="Karya Siddhi Hanuman Temple")
+
+idp = FastAPIKeycloak(
+    server_url="http://localhost:8890/",
+    client_id="ksht_client",
+    client_secret="7vF3BOpO5NJC6vKQ8RT8qppNAVdLXDiw",
+    admin_client_secret="ZUVI3IQXBDxNqPml2dunQ5wpP5TD4pXZ", 
+    realm="ksht_realm", 
+    callback_uri="http://localhost:8009/"
+)
+idp.add_swagger_config(app)
 
 
 @app.get("/")
@@ -23,3 +36,16 @@ async def startup():
 async def shutdown():
     if database.is_connected:
         await database.disconnect()
+
+@app.get("/identity-providers", tags=["admin-cli"])
+def get_identity_providers():
+    return idp.get_identity_providers()
+
+
+@app.get("/idp-configuration", tags=["admin-cli"])
+def get_idp_config():
+    return idp.open_id_configuration
+
+@app.get("/login")
+def login_redirect():
+    return RedirectResponse(idp.login_uri)
